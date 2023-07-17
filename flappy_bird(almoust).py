71100f60245b12b6c6b7095c736a536561dcde69
Marina Dubko <75209870+lillyroot@users.noreply.github.com>
2023-07-17 20:36:29 +0500

    Update flappy_bird(almoust).py
    убрала функцию текста

--- flappy_bird(almoust).py
+++ flappy_bird(almoust).py
@@ -27,15 +27,6 @@ def tubes_position():
     tube1.y = -300
     tube2.y = 400
 
-
-def draw_text(surf, text, size, x, y):
-    font = pygame.font.Font(font_name, size)
-    text_surface = font.render(text, True, 'white')
-    text_rect = text_surface.get_rect()
-    text_rect.midtop = (x, y)
-    surf.blit(text_surface, text_rect)
-
-
 while playing:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
